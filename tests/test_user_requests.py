54dc321c69244db8fe4d044d914b00eff5bd0b3c
Roman_A <Justrom21@gmail.com>
2023-11-27 00:18:53 +0500

    Add contract and get user list tests

--- tests/test_user_requests.py
+++ tests/test_user_requests.py
@@ -3,6 +3,4 @@ from jsonschema import validate
 
 def test_list_users():
     response = httpx.get('https://reqres.in/api/users?page=2')
-    #json_response = response.json()
-    #print(json_response['data'])
     assert response.status_code == 200
