375fd41b16d8455fe4e9831774ab7919734c1fe0
Roman_A <Justrom21@gmail.com>
2023-12-20 20:00:37 +0500

    откат главной ветки

--- tests/test_work_with_user_data.py
+++ tests/test_work_with_user_data.py
@@ -1,7 +1,10 @@
+import json
+import pytest
 import httpx
 from core.contracts import USER_CREATE_DATA_SCHEMA
 from jsonschema import validate
 import allure
+import datetime
 
 BASE_URL = 'https://reqres.in/api/users/'
 
@@ -14,14 +17,15 @@ def test_delete_user():
     with allure.step('Проверяем код ответа'):
         assert response.status_code == 204
 
+json_file = open('./user_credentials.json')
+users_credentials = json.load(json_file)
+
+@pytest.mark.parametrize("user_credentials", users_credentials)
 @allure.title('Метод создания данных пользователя')
-def test_create_user():
+def test_create_user(user_credentials):
     headers = {'Content-Type': 'Application/json'}
-    Name = "Roman"
-    Job = "QA"
-    body = {"name": Name,"job": Job}
     with allure.step('Создание пользователя'):
-        response = httpx.post(BASE_URL, json=body, headers=headers)
+        response = httpx.post(BASE_URL, json=user_credentials, headers=headers)
 
     with allure.step('Проверяем код ответа'):
         assert response.status_code == 201
@@ -30,8 +34,7 @@ def test_create_user():
     with allure.step('Сверяем ответ с контрактом'):
         validate(resources_data, USER_CREATE_DATA_SCHEMA)
 
-    with allure.step('Проверяем переданное имя'):
-        assert response.json()['name'] == Name
-
-    with allure.step('Проверяем переданную должность'):
-        assert response.json()['job'] == Job
+    response_data = response.json()['createdAt'].replace('T',' ')
+    current_data = str(datetime.datetime.utcnow())
+    with allure.step('Проверка времени создания'):
+        assert response_data[0:16] == current_data[0:16]
